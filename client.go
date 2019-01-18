781f44d13288e20ad5ca6f9a479aa527d2547c11
placeless <placeless@catchme.com.cn>
2019-01-18 16:31:50 +0800

    使用更加随机的字符串生成函数

--- client.go
+++ client.go
@@ -1,16 +1,16 @@
 package iot
 
 import (
-	"net/url"
-	"strings"
-	"time"
-	"math/rand"
-	"sort"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
-	"net/http"
 	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
 )
 
 type Client struct {
@@ -26,7 +26,7 @@ func (c *Client) Send(request map[string]string) (string, error) {
 	request["SignatureMethod"] = "HMAC-SHA1"
 	request["Timestamp"] = time.Now().UTC().Format(time.RFC3339)
 	request["SignatureVersion"] = "1.0"
-	request["SignatureNonce"] = GetRandomString(14)
+	request["SignatureNonce"] = RandStringBytesMaskImprSrc(14)
 	request["RegionId"] = c.RegionId
 	request["AccessKeyId"] = c.AccessKeyId
 
@@ -80,4 +80,30 @@ func GetRandomString(l int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandStringBytesMaskImprSrc(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return string(b)
 }
--- client_test.go
+++ client_test.go
@@ -4,10 +4,28 @@ import (
 	"testing"
 )
 
-func TestRandStr(t *testing.T) {
-	if len(GetRandomString(12)) == 12{
-		t.Log("测试通过")
-	}else {
-		t.Error("测试不通过")
+func TestRandStringBytesMaskImprSrc(t *testing.T) {
+	m := make(map[string]string)
+	ch := make(chan string, 100)
+	for i := 0; i < 100; i++ {
+		go genRand(ch)
+	}
+	for i := 0; i < 100; i++ {
+		s := <-ch
+		m[s] = s
+	}
+	if len(m) == 100 {
+		t.Log("同一时刻生成随机字符串重复测试成功")
+	} else {
+		t.Error("同一时刻生成随机字符串重复测试失败")
+	}
+}
+func genRand(ch chan string) {
+	ch <- RandStringBytesMaskImprSrc(14)
+}
+
+func BenchmarkRandStringBytesMaskImprSrc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
 	}
 }
